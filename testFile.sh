9a295556960fcaf1ccd9b8a200c4f1249e8cd770
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-03-02 11:16:44 +0200

    Shell Script looks good

---
+++ testFile.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+MAX_CITIZENS=10
+numLines=$3
+
+if (( $numLines > $MAX_CITIZENS )); then
+    if [ "$4" != "duplicatesAllowed" ]; then
+        echo "Cannot create $numLines records without duplicates."
+        echo "Up to $MAX_CITIZENS different citizens can be created."
+        echo "Re-run with numLines <= $MAX_CITIZENS or use duplicatesAllowed flag."
+        echo "Aborting."
+        exit 1
+    fi
+fi
+
+for ((i=0; i <= MAX_CITIZENS; i++)) do
+    citizens[i]=""
+done
+
+numViruses=0
+# Reading Virus names
+while read -r virus; do
+    viruses[numViruses]="$virus"
+    let "numViruses++"
+done < "$1"
+
+numCountries=0
+# Reading Country names
+while read -r country; do
+    countries[numCountries]="$country"
+    let "numCountries++"
+done < "$2"
+
+
+lineCount=0
+name="name"
+surname="surname"
+date="3-2-2021"
+
+vaccinate=("YES", "NO")
+
+while [ "$lineCount" != "$numLines" ]; do
+    id=$RANDOM
+    let "id %= $MAX_CITIZENS"
+    if [ "${citizens[id]}" == "" ]; then
+        # Creating new citizen info
+        #--------------------------
+        # TODO: Create random name & surname
+        #--------------------------
+        country_id=$RANDOM
+        let "country_id %= $numCountries"
+        age=$RANDOM
+        let "age %= 120"
+        citizens[id]="${id} ${name}_${id} ${surname}_${id} ${countries[country_id]} ${age}"
+    else
+        if [ "$4" != "duplicatesAllowed" ]; then
+            continue
+        fi
+    fi
+    virus_id=$RANDOM
+    let "virus_id %= $numViruses"
+    vaccinated=$RANDOM
+    let "vaccinated %= 2"
+    printf '%s %s ' "${citizens[id]}" "${viruses[virus_id]}"
+    if [ "$vaccinated" == "0" ]; then
+        printf "YES %s\n" "$date"
+    else
+        printf "NO\n"
+    fi
+    let "lineCount++"
+done > "inputFile"
+
+exit 0
