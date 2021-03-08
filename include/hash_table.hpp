1d2cb07fc7b6e88b94daf0e471881975f4a64483
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-03-08 17:02:52 +0200

    Hash Table stuff

--- include/hash_table.hpp
+++ include/hash_table.hpp
@@ -8,14 +8,17 @@ class LinkedList;
 class HashTable
 {
     private:
-        int size;
+        unsigned int size;
         LinkedList **buckets;
         CompareFunc compare;
         DestroyFunc destroy;
+
+        unsigned int getHashCode(void *data, unsigned int mod);
         
     public:
         HashTable(int num_buckets, CompareFunc comp, DestroyFunc dest);
         ~HashTable();
+        bool insert(void *element, void **present);
 };
 
 #endif
--- lib/hash_table.cpp
+++ lib/hash_table.cpp
@@ -1,4 +1,7 @@
 #include <cstddef>
+#include <cstdlib>
+#include <cstring>
+#include <openssl/sha.h>
 #include "../include/hash_table.hpp"
 #include "../include/linked_list.hpp"
 
@@ -21,4 +24,26 @@ HashTable::~HashTable()
         }
         delete[] buckets;
     }
+}
+
+unsigned int HashTable::getHashCode(void *data, unsigned int mod)
+{
+    int result;
+
+    // Initial hashcode is stored here
+    unsigned char *hash = (unsigned char*)malloc(SHA_DIGEST_LENGTH);
+    unsigned long int hash_num;
+
+    // Getting the hashcode
+    SHA1((unsigned char*)data, sizeof(int), hash);
+
+    // Copying part of it and storing it
+    memcpy(&hash_num, hash, sizeof(unsigned int));
+
+    free(hash);
+
+    // Calculating modulo and returning the result in a typical int
+    hash_num = hash_num % mod;
+    result = hash_num;
+    return result;
 }
