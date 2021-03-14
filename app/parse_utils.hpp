98a9884057e7137840ab921cf02e2fb403079c45
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-03-14 19:36:23 +0200

    Parsing...

--- app/parse_utils.hpp
+++ app/parse_utils.hpp
@@ -3,7 +3,20 @@
 
 bool isPositiveNumber(const char* str);
 char* fgetline(FILE *stream);
+
 bool insertCitizenRecordParse(int &citizen_id, char *&citizen_fullname, char *&country_name,
                               int &citizen_age, char *&virus_name, bool &vaccinated, Date &date);
+bool vaccinateNowParse(int &citizen_id, char *&citizen_fullname, char *&country_name,
+                       int &citizen_age, char *&virus_name);
+bool vaccineStatusBloomParse(int &citizen_id, char *&virus_name);
+
+// 2 cases here! we may not get a virus name
+bool vaccineStatusParse(int &citizen_id, char *&virus_name);
+
+// Again, 2 cases! we may not get a country name.
+bool populationStatusParse(char *&country_name, char *&virus_name, Date &start, Date &end);
+bool popStatusByAgeParse(char *&country_name, char *&virus_name, Date &start, Date &end);
+
+bool listNonVaccinatedParse(char *&virus_name);
 
 #endif
